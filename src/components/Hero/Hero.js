a4f38db05c02da0c6cbc60cd28a16704239c4011
chathu_08 <chathu.rash97@gmail.com>
2021-12-26 07:55:49 +0530

    Completed the Hero and Projects components

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,9 +4,24 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
+const Hero = () => (
   <div>
-    Hero
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Hello there! <br />
+          I'm Chathu :)
+        </SectionTitle>
+
+        <SectionText>
+          An undergraduate  |  A developer  |  A UI/UX designer
+        </SectionText>
+
+        <Button onClick={() => window.location = 'https://www.linkedin.com/in/chathumi-kumarapeli-b66598214/?msgControlName=reply_to_sender&msgConversationId=2-NmQzZTc2ZWUtMjk2NC00OWVkLWExOTgtMmQ0M2RiMjE4ODgwXzAxMA%3D%3D&msgOverlay=true'}>
+          Learn More...
+        </Button>
+      </LeftSection>
+    </Section>
   </div>
 );
 
