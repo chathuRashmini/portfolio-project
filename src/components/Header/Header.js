4f09ce2bf80ba4a439098c7856d32712e86e1344
chathu_08 <chathu.rash97@gmail.com>
2021-12-26 07:11:16 +0530

    Completed the header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,53 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href='/'>
+        <a style={{ display: 'flex', alignItems: 'center', color: 'white', marginBottom: '20px' }}>
+          <DiCssdeck size='3rem' />
+          <Span>Portfolio</Span>
+        </a>
+      </Link>
+    </Div1>
+
+    <Div2>
+      <li>
+        <Link href='#projects'>
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+
+      <li>
+        <Link href='#technologies'>
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+
+      <li>
+        <Link href='#about'>
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+
+    <Div3>
+      <SocialIcons href='https://github.com/chathuRashmini'>
+        <AiFillGithub size='3rem' />
+      </SocialIcons>
+
+      <SocialIcons href='https://www.linkedin.com/in/chathumi-kumarapeli-b66598214/?msgControlName=reply_to_sender&msgConversationId=2-NmQzZTc2ZWUtMjk2NC00OWVkLWExOTgtMmQ0M2RiMjE4ODgwXzAxMA%3D%3D&msgOverlay=true'>
+        <AiFillLinkedin size='3rem' />
+      </SocialIcons>
+
+      <SocialIcons href='https://instagram.com'>
+        <AiFillInstagram size='3rem' />
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
